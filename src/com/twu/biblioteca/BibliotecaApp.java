cb0ddd9f361a78837074c604a7423ddbdf93a69b
Megan Goodling <megan.goodling@thoughtworks.com>
2019-08-15 09:48:33 -0500

    Welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,6 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Ok last time around");
+        System.out.println("Welcome to Biblioteca!!!");
     }
 }
