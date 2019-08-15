e8243248df18db577ac5fe0ad13d23c8fe1c5f86
megangoodling <54111925+megangoodling@users.noreply.github.com>
2019-08-15 09:35:06 -0500

    Update BibliotecaApp.java

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,6 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hey this is a test!");
+        System.out.println("Let's try that again!");
     }
 }
