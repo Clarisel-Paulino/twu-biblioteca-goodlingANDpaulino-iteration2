d6c73fa001230be75de7c686b6232092045d2765
Clarisel Paulino <clariselpaulino@Clarisels-MacBook-Pro.local>
2019-08-19 14:47:24 -0500

    Made Menu return dynamic menu options based on user log in status

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -2,20 +2,51 @@ package com.twu.biblioteca;
 
 public class Menu {
 
-    private String menuOptions;
     private ScannerWrapper scanner;
 
     // User input for menu option
     private int menuSelection;
+    private boolean userLoggedIn;
 
     public Menu(ScannerWrapper scanner) {
-        this.menuOptions = "\nSelect an option from the menu below:\n 1. See List of Books\n" +
-                " 2. See List of Movies\n 3. Return Book\n 4. Return Movie\n 5. Exit Biblioteca\n";
         this.scanner = scanner;
+        this.userLoggedIn = false;
     }
 
+    /**
+     * getMenuOptions
+     * @return menu options as string based on whether or not user is logged in
+     */
     public String getMenuOptions() {
-        return menuOptions;
+        StringBuilder sb = new StringBuilder();
+
+        // Initial Menu Options
+        sb.append("\nSelect an option from the menu below:\n 1. See List of Books\n" +
+                " 2. See List of Movies\n 3. Return Book\n 4. Return Movie\n 5. Exit Biblioteca\n");
+        
+        // User is logged in
+        if(userLoggedIn){
+            sb.append("6. See My Account Info\n");
+        }
+        // User is not logged in
+        else {
+            sb.append("6. Log In\n");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * sets userLoggedIn boolean to True
+     */
+    public void setUserLoggedIn(){
+        this.userLoggedIn = true;
+    }
+
+    /**
+     * sets userLoggedIn boolean to False
+     */
+    public void setUserLoggedOut(){
+        this.userLoggedIn = false;
     }
 
     /**
